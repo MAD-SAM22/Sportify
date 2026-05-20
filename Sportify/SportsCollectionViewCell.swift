d785985e8075fc1b8b870146f3394f3a8e9f1803
osama hosam abdellatif <osama.abdellatif.official@hotmail.com>
2026-05-20 13:54:57 +0300

    home page sports collection view with images and UI

---
+++ Sportify/SportsCollectionViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  SportsCollectionViewCell.swift
+//  Sportify
+//
+//  Created by Osama Hosam on 20/05/2026.
+//
+
+import UIKit
+
+struct SportsHome {
+    let name: String
+    let imageName: String
+}
+
+class SportsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var sportImageView: UIImageView!
+    @IBOutlet weak var sportLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        // Round the entire cell
+        layer.cornerRadius = 14
+        layer.masksToBounds = false
+
+        // Shadow on the cell layer
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 6
+
+        // Image fills the cell completely
+        sportImageView.layer.cornerRadius = 14
+        sportImageView.layer.masksToBounds = true
+        sportImageView.contentMode = .scaleAspectFill
+
+        // Style the label to sit as an overlay at the bottom
+        sportLabel.textColor = .white
+        sportLabel.font = UIFont.boldSystemFont(ofSize: 17)
+        sportLabel.textAlignment = .center
+
+        // Dark gradient-like background behind the label
+        sportLabel.backgroundColor = UIColor.black.withAlphaComponent(0.45)
+        sportLabel.layer.cornerRadius = 0  // flush with cell bottom
+        sportLabel.clipsToBounds = true
+    }
+}
--- Sportify/ViewController.swift
+++ Sportify/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let sportsData: [SportsHome] = [
+        SportsHome(name: "Basketball", imageName: "basketball_img"),
+        SportsHome(name: "Soccer", imageName: "soccer_img"),
+        SportsHome(name: "Tennis", imageName: "tennis_img"),
+        SportsHome(name: "Football", imageName: "football_img"),
+        SportsHome(name: "Swimming", imageName: "swimming_img"),
+        SportsHome(name: "Cycling", imageName: "cycling_img")
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.contentInset = UIEdgeInsets(top: 40, left: 0, bottom: 0, right: 0)
+
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.minimumInteritemSpacing = 12
+            layout.minimumLineSpacing = 16
+        }
     }
 
-
-}
-
+    // MARK: - UICollectionViewDataSource
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return sportsData.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeSportsCell", for: indexPath) as! SportsCollectionViewCell
+            
+            let sport = sportsData[indexPath.item]
+            cell.sportLabel.text = sport.name
+            cell.sportImageView.image = UIImage(named: sport.imageName)
+            
+            return cell
+        }
+        
+        // MARK: - UICollectionViewDelegateFlowLayout (Grid Math)
+        
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sideInset: CGFloat = 12    // must match insetForSectionAt left/right
+        let gap: CGFloat = 12          // gap between the two columns
+        let availableWidth = collectionView.frame.width - (sideInset * 2) - gap
+        let side = availableWidth / 2
+        return CGSize(width: side, height: side * 1.2)
+    }
+        
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 12, bottom: 16, right: 12)
+    }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 16 // Vertical space between rows
+        }
+    }
